72072c73e6c2e8fad61d00dcf9e8ca97f2fbbb78
juanDaRestrepo <jdrestrepo@unitecnica.net>
2022-02-10 17:59:47 -0500

    installation of angular and creation of cards component and card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CardsComponent } from './cards/cards.component';
+import { CardComponent } from './card/card.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,CardsComponent, CardComponent
   ],
   imports: [
     BrowserModule,
