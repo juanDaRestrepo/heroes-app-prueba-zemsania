0bfc42014065e0baf36860f3abc97419e74e2f03
juanDaRestrepo <jdrestrepo@unitecnica.net>
2022-02-10 19:53:03 -0500

    changing of way to charge bootstrap and creating the array of heroes

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,9 +1,9 @@
 import {Component} from "@angular/core";
-
+import { heroes } from "./heroes.model";
 @Component({
     selector: 'app-cards',
     templateUrl: './cards.component.html'
 })
 export class CardsComponent{
-
+    heroes = heroes;
 }
