358468b006ed22dbade052f8b6d1f6fa134b0f1f
juanDaRestrepo <jdrestrepo@unitecnica.net>
2022-02-10 23:28:44 -0500

    Creation of CarrouselComponent and configuration of routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule,Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
-import { CardComponent } from './card/card.component'
+import { CardComponent } from './card/card.component';
+import { CarrouselComponent } from './carrousel/carrousel.component'
+
+const routes: Routes = [
+  {path: '', component: CardsComponent},
+  {path: 'carrousel', component: CarrouselComponent}
+]
+
 @NgModule({
   declarations: [
-    AppComponent,CardsComponent, CardComponent
+    AppComponent,CardsComponent, CardComponent, CarrouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      routes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
