bbdebbea7b64c531607b874ef2a4aca063128d23
juanDaRestrepo <jdrestrepo@unitecnica.net>
2022-02-12 12:46:13 -0500

    Heroku config finished

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/org.apache.cordova.newapp'));
+app.use(express.static(__dirname + '/dist/heroes-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/heroes-app/org.apache.cordova.newapp'));
+res.sendFile(path.join(__dirname+'/dist/heroes-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
